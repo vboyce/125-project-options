1dcd21873a148bafa5c1d413aa1028e711ddf896
vboyce <vero.boyce@gmail.com>
2024-01-01 14:26:40 -0800

    work on sloman

---
+++ sloman_2016/experiment/instructions.js
@@ -0,0 +1,16 @@
+
+const WELCOME_INSTRUCTION = "<p> welcome and consent go here </p>"
+
+const PRE_TEST_INSTRUCTION = 
+    "<p> This experiment is about a variety of recent scientific discoveries. We will ask you to rate <b>how well you feel you understand</b> the way these things work using a seven-point rating scale. Before you get started, we need to tell you what the scores on the rating scale reflect. Below are three different descriptions of knowledge about an object that is well understood, the crossbow. Please read each description to get a sense of how to use the scale. As you will see, a 7 implies detailed and deep knowledge, a 1 implies very little knowledge, and a 4 is in the middle. </p>"+
+"<p><b>Level 7 knowledge:</b> A person with level 7 knowledge of crossbows can tell you all about their parts and how they work together. This person could tell you that a crossbow has a stiff, flexible piece of metal as a bow with a wire or strong line; that the bow is permanently mounted on a block of wood or metal; and that the wire is pulled back by something that gives a mechanical advantage—either a lever, a small block and tackle, or a crank wound around a spool that pulls a wire attached to the bow wire. The bow wire is held back by a pin connected to a trigger, and an arrow is set in front of it. The pin is directly connected to the trigger so that when you pull on the trigger, it causes the pin to pivot around a point such that the end moves downwards and releases the bow wire. When the pin releases the string, the bow very quickly un-flexes, rapidly imparting the energy stored in the flexed bow to the arrow.</p>"+
+
+"<p><b>Level 4 knowledge:</b> A person with level 4 knowledge might know that the crossbow is a fixed bow and arrow arrangement; that it gets more power than a normal bow and arrow because it allows you to pull the string back extra hard and then trap it there, rather than hold it; and that it is then released by a trigger.</p>"+
+
+"<p><b>Level 1 knowledge:</b> A person with level 1 knowledge might know what a crossbow looks like and what it does (shoots arrows).</p>"+
+
+"<p>Please go through the following items and select the number from 1 to 7 that best indicates your understanding of the phenomena. There are no right or wrong answers, as the study is about your sense of your own understanding — please give us your first impression. </p>";
+
+const POST_TEST_INSTRUCTION =
+    "<h1>End of the experiment.</h1>"                                   +
+    "<h2>Many thanks for participating</h2>";
